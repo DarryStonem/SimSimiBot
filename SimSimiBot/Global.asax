cd454fa05e888d85e059b538a15ffe92b1545d70
Cristian González <prototipo_fauster@hotmail.com>
2016-05-07 15:04:54 -0500

    Add project files.

---
+++ SimSimiBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimSimiBot.WebApiApplication" Language="C#" %>
---
+++ SimSimiBot/Services/SimSimiClient.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using SimSimiBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimSimiBot.Services
+{
+    public class SimSimiClient
+    {
+        /// <summary>
+        /// Use your API KEY from the SimSimi Portal
+        /// </summary>
+        private const string SimSimiKey = "YOUR_API_KEY";
+
+
+        public async Task<string> SimSimiRequest(string message)
+        {
+            var request = $"http://sandbox.api.simsimi.com/request.p?key={SimSimiKey}&lc=es&ft=1.0&text={message}";
+            var httpResponse = await new HttpClient().GetAsync(request);
+
+            if (!httpResponse.IsSuccessStatusCode)
+                return String.Empty;
+
+            var response = await httpResponse.Content.ReadAsStringAsync();
+
+            var simSimi = JsonConvert.DeserializeObject<SimSimiResponse>(response);
+            if (simSimi.Result == 100)
+                return simSimi.Response;
+
+            return String.Empty;
+        }
+    }
+}
